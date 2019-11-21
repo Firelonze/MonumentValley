d904578559f90599d45fa913cedf8732ea64c8cc
WoolfyDuh <14461196+WoolfyDuh@users.noreply.github.com>
2019-11-21 11:58:21 +0100

    made rotator
    added rotator

---
+++ Monument Valley VS/Assets/RotatingScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingScript : MonoBehaviour
+{
+	Vector3 myVector = new Vector3(0.0f, 1.0f, 0.0f);
+	private float angle = 6;
+
+	private void OnMouseDrag()
+	{
+		float x = Input.GetAxis("Mouse X");
+		transform.RotateAround(transform.position, myVector*Time.deltaTime*-x,angle);
+	}
+}
