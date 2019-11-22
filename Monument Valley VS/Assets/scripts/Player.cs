dada274197f95b193e60b12eb4b3628356694283
T3rabyte <teun.bruin@hotmail.com>
2019-11-22 18:27:38 +0100

    First commit
    player can click on specific locations and walk to it

---
+++ Monument Valley VS/Assets/scripts/Player.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject currentWaypointObject;
+    public float step = 2f;
+    public Vector3 currentWaypointTransform;
+
+    public int walkIndex = 0;
+    public int destinationInt;
+
+    public bool omhoog;
+
+    public int currentWaypoint;
+
+    public bool moving = false;
+
+    void Update()
+    {
+        if (GameObject.Find("Selection Manager").GetComponent<selectionManager>().destination != 0 && moving == false)
+        {
+            moving = true;
+            destinationInt = GameObject.Find("Selection Manager").GetComponent<selectionManager>().destination;
+            Debug.Log(destinationInt);
+            GetNextWaypoint();
+        }
+
+        if (omhoog == true)
+        {
+            if (walkIndex >= destinationInt)
+            {
+                GameObject.Find("Selection Manager").GetComponent<selectionManager>().destination = 0;
+
+                if (Vector3.Distance(currentWaypointTransform, this.transform.position) < 0.01)
+                {
+                    moving = false;
+                }
+            }
+        }
+        if (omhoog == false)
+        {
+            if (walkIndex <= destinationInt)
+            {
+                GameObject.Find("Selection Manager").GetComponent<selectionManager>().destination = 0;
+
+                if (Vector3.Distance(currentWaypointTransform, this.transform.position) < 0.01)
+                {
+                    moving = false;
+                }
+            }
+        }
+
+        if (moving == true)
+        {
+            if (Vector3.Distance(currentWaypointTransform, this.transform.position) < 0.01)
+            {
+                if (omhoog == true)
+                {
+                    walkIndex++;
+                }
+                if (omhoog == false)
+                {
+                    walkIndex--;
+                }
+                GetNextWaypoint();
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, currentWaypointTransform, step);
+            transform.LookAt(currentWaypointTransform);
+        }
+    }
+
+    public void GetNextWaypoint()
+    {
+        if (destinationInt >= walkIndex)
+        {
+            omhoog = true;
+        }
+        if (destinationInt <= walkIndex)
+        {
+            omhoog = false;
+        }
+        currentWaypoint = walkIndex;
+        currentWaypointObject = GameObject.Find("" + currentWaypoint);
+        currentWaypointTransform = new Vector3(currentWaypointObject.transform.position.x, 1.048f, currentWaypointObject.transform.position.z);
+    }
+}
---
+++ Monument Valley VS/Assets/scripts/SelectableInt.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectableInt : MonoBehaviour
+{
+    public int waypointInt;
+    void Start()
+    {
+        switch (name)
+        {
+            case "1":
+                waypointInt = 1;
+                break;
+            case "2":
+                waypointInt = 2;
+                break;
+            case "3":
+                waypointInt = 3;
+                break;
+            case "4":
+                waypointInt = 4;
+                break;
+            case "5":
+                waypointInt = 5;
+                break;
+            case "6":
+                waypointInt = 6;
+                break;
+            case "7":
+                waypointInt = 7;
+                break;
+            case "8":
+                waypointInt = 8;
+                break;
+            case "9":
+                waypointInt = 9;
+                break;
+        }
+    }
+}
