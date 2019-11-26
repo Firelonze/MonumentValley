896831817199644511e38ac726741dda26c2dcd2
WoolfyDuh <14461196+WoolfyDuh@users.noreply.github.com>
2019-11-26 09:57:50 +0100

    Removed Leg Clamp & Rotator smoothes out mostly
    Removed Leg Clamp
    Rotator now Rotates "Smoothly"
    
    Rotator is in Debug & Polish fase

--- Monument Valley VS/Assets/Scripts/RotatingScript.cs
+++ Monument Valley VS/Assets/Scripts/RotatingScript.cs
@@ -1,18 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class RotatingScript : MonoBehaviour //LegClamp 
+public class RotatingScript : MonoBehaviour 
 {
-	Vector3 myVector = new Vector3(0.0f, 1.0f, 0.0f);
+	private Vector3 myVector = new Vector3(0.0f, 1.0f, 0.0f);
 	private float angle = 3.2f;
-//	private LegClamp leg1;//
-//	private LegClamp leg2	;
-	private void Awake()
-	{
-//		leg1 = GameObject.Find("Leg1").GetComponent<LegClamp>();
-//		leg2 = GameObject.Find("Leg2").GetComponent<LegClamp>();
-	
-	}
+
 	private void OnMouseDrag()
 	{
 		float x = Input.GetAxis("Mouse X");
@@ -21,8 +14,6 @@ public class RotatingScript : MonoBehaviour //LegClamp
 
 	private void OnMouseUp()
 	{
-		//	leg1.canSnap = true;
-		//	leg2.canSnap = true;
 		float snapRotationAngle = (Mathf.RoundToInt(transform.rotation.eulerAngles.y / 90)) * 90;
 		StartCoroutine(RotateToSnapPoint(snapRotationAngle));
 
@@ -30,18 +21,16 @@ public class RotatingScript : MonoBehaviour //LegClamp
 
 	private IEnumerator RotateToSnapPoint(float snapRotation)
 	{
-		/*
-		 * current rotation niet in de buurt zit van snapRotation
-		 * rotate richting snaprotation
-		while ???
-			{
-
+		float dist = 0;
+		while (dist < 1)
+		{
+			dist += Time.deltaTime;
+			float currentRot = Mathf.Lerp(transform.rotation.eulerAngles.y, snapRotation, dist);
+			transform.rotation = Quaternion.Euler(0, currentRot, 0);
 			yield return null;
-			transform.rotation = Quaternion.Euler(0,snapRotation,0);
+			
 		}
-		*/
-		yield return null;
-		transform.rotation = Quaternion.Euler(0, snapRotation, 0);
+		Debug.Log("Coroutine ended");
 	}
 
 }
